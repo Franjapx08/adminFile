c08e916d4571c16a856821ab35beca13c288176b
Frankx08 <you@example.com>
2019-11-25 01:25:08 -0700

    updateFinish

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
-Vue.component("modal", {
-  template: "#modal-template"
-});
+/* Variables globales de libreria NODE JS para la administracio nde archivos */
+var fs = require("fs");
+var electron = require("electron");
+var ruta = require("path");
+var fsxtra = require("fs-extra");
 
 var App = new Vue({
   el: "#app",
@@ -8,7 +10,7 @@ var App = new Vue({
     return {
       estado: ["Raiz"],
       datos: [],
-      link: ["/Users/franj/Proyectos/Pruebas"],
+      ruta: ["/Users/franj/Proyectos/Pruebas"],
       anterior: [],
       button: true,
       showModal: false,
@@ -24,61 +26,70 @@ var App = new Vue({
     };
   },
   created() {
-    //this.estado = this.link[0];
-    fs.readdirSync(this.link.join("/")).forEach((e, i) => {
-      this.datos[i] = {};
+    /* Funcion que sol ose ejecuta una ves cuando inicia el programa */
+    fs.readdirSync(this.ruta.join("/")).forEach((e, i) => {
+      //for que recorre los archivos de la ruta especificada
+      this.datos[i] = {}; //en varaible global datos que es la que se meustra en el programa se agregan los datos que encontro en el for
       this.datos[i].nombre = e;
-      if (fs.statSync(this.link.join("/") + "/" + e).isDirectory()) {
+      if (fs.statSync(this.ruta.join("/") + "/" + e).isDirectory()) {
         this.datos[i].tipo = "Directorio";
       } else {
         this.datos[i].tipo = "Archivo";
       }
-      this.datos[i].size = fs.statSync(this.link.join("/") + "/" + e).size;
+      this.datos[i].size = fs.statSync(this.ruta.join("/") + "/" + e).size;
       this.datos[i].fecha = fs
-        .statSync(this.link.join("/") + "/" + e)
+        .statSync(this.ruta.join("/") + "/" + e)
         .ctime.toString();
       //console.log(e);
-      //console.log(fs.statSync(this.link.join("/") + "/" + e).ctime);
+      //console.log(fs.statSync(this.ruta.join("/") + "/" + e).ctime);
     });
     // console.log(this.datos);
   },
   watch: {
-    link() {
-      this.datos = [];
-      fs.readdirSync(this.link.join("/")).forEach((e, i) => {
-        this.datos[i] = {};
-        this.datos[i].nombre = e;
-        if (fs.statSync(this.link.join("/") + "/" + e).isDirectory()) {
+    //funciones que cambian en tiempo real
+    ruta() {
+      //variblae globar que camvbia en tiempo real cuando es utilizada
+      this.datos = []; //inicializa arreglo datos
+      fs.readdirSync(this.ruta.join("/")).forEach((e, i) => {
+        //funcion que busca todos los archivos en la pc
+        this.datos[i] = {}; //conmvierte en JSON cada resultado
+        this.datos[i].nombre = e; //agrega nombre
+        if (fs.statSync(this.ruta.join("/") + "/" + e).isDirectory()) {
+          //funcion que identifica si es carpeta o no
           this.datos[i].tipo = "Directorio";
         } else {
           this.datos[i].tipo = "Archivo";
         }
-        this.datos[i].size = fs.statSync(this.link.join("/") + "/" + e).size;
-        this.datos[i].fecha = fs
-          .statSync(this.link.join("/") + "/" + e)
-          .atime.toString();
+        this.datos[i].size = fs.statSync(this.ruta.join("/") + "/" + e).size; //agrega tamaño del archivo o carpeta
+        this.datos[i].fecha = fs //fecha en que fue creado
+          .statSync(this.ruta.join("/") + "/" + e)
+          .atime.toString(); //convierte resultado en tipoString para poder visualizarce
       });
-      console.log(this.datos);
-      console.log(this.link);
     }
   },
   methods: {
     buscar() {
-      var encontraro = false;
+      //funcion para buscar en campo busqueda
+      var encontraro = false; //variable para identificar si fue encontrado o no
       if (this.formBusqueda != null) {
+        //si el campo de texto tiene texto
         /* Hacer la busqueda */
-        var buscar = this.formBusqueda;
+        var buscar = this.formBusqueda; // texto en campo texto
         for (i in this.datos) {
+          //for que recorre arreglo de datos
           if (buscar == this.datos[i].nombre) {
+            //si lo que busca esta en los datos
             if (this.datos[i].tipo == "Directorio") {
-              this.link.push(this.datos[i].nombre);
+              //si es directorio abre el directorio
+              this.ruta.push(this.datos[i].nombre);
               this.estado.push(this.datos[i].nombre);
               this.formBusqueda = null;
               encontraro = true;
             } else {
+              //si es archivo lo abre
               encontraro = true;
               electron.shell.openItem(
-                ruta.join(this.link.join("/"), this.datos[i].nombre)
+                ruta.join(this.ruta.join("/"), this.datos[i].nombre)
               );
             }
           }
@@ -89,61 +100,72 @@ var App = new Vue({
       }
     },
     clickRow(index) {
-      this.formBusqueda = null;
+      this.formBusqueda = null; //elimina si hay datos en campo texto
       if (index.tipo != "Archivo") {
+        //si es carpeta
         this.button = false;
-        this.link.push(index.nombre);
+        this.ruta.push(index.nombre);
         this.estado.push(index.nombre);
       } else {
-        electron.shell.openItem(ruta.join(this.link.join("/"), index.nombre));
+        //si es archivo
+        electron.shell.openItem(ruta.join(this.ruta.join("/"), index.nombre));
       }
     },
     backPage() {
-      if (this.link.length > 1) {
-        this.anterior = this.link;
-        this.link.pop();
+      //rompe la pila y muestra los datos anteriores
+      if (this.ruta.length > 1) {
+        this.anterior = this.ruta;
+        this.ruta.pop();
         this.estado.pop();
       }
     },
     up() {
+      //lleva a la carpeta raiz
       this.estado = ["Raiz"];
       this.datos = [];
-      this.link = ["/Users/franj/Proyectos"];
+      this.ruta = ["/Users/franj/Proyectos"];
     },
     recargar() {
+      //vuelve a recargar los datos
       this.formBusqueda = null;
-      let x = this.link;
-      this.link = [];
-      this.link = x;
+      let x = this.ruta;
+      this.ruta = [];
+      this.ruta = x;
     },
     nuevaCarpeta() {
       if (this.formCarpeta != null) {
-        var nombre = this.formCarpeta;
-        fs.mkdir(this.link.join("/") + "/" + nombre, err => {
+        //si el campo del nombre carpeta no esta vacio
+        var nombre = this.formCarpeta; //texto del campo nombre carpeta
+        fs.mkdir(this.ruta.join("/") + "/" + nombre, err => {
+          //funcion que crea carpeta
           if (!err) {
+            //si no hay error
             document.getElementById("id01").style.display = "none";
             this.recargar();
             this.formCarpeta = null;
           } else {
+            //si hay error
             alert("Error! \n" + "Error");
           }
         });
       } else {
+        //si el campo de texto esta vacio
         document.getElementById("id01").style.display = "none";
       }
     },
     nuevoArchivo() {
       if (this.formArchivo != null) {
+        //si el campo de texto no esta vacio
         var nombre = this.formArchivo;
         //funcion para ver si existe el archivo con ese nomre
-        fs.open(this.link.join("/") + "/" + nombre, (err, fd) => {
+        fs.open(this.ruta.join("/") + "/" + nombre, (err, fd) => {
           if (err) {
             //no existe
-            fs.writeFile(this.link.join("/") + "/" + nombre, "", err => {});
+            fs.writeFile(this.ruta.join("/") + "/" + nombre, "", err => {});
             document.getElementById("id02").style.display = "none"; //oculta modal
             this.recargar(); //recarga
             this.formArchivo = null; //campo de texto volver a nulo
-            console.log(this.link);
+            console.log(this.ruta);
           } else {
             // alert("Error! \n" + "Nombre en uso");
             document.getElementById("id02").style.display = "none";
@@ -151,10 +173,12 @@ var App = new Vue({
           }
         });
       } else {
+        // si el campo de texto esta vacio
         document.getElementById("id02").style.display = "none";
       }
     },
     cancelar() {
+      //funcuion global que cancela todos y cierra los modales y borra las variables de textos
       document.getElementById("id01").style.display = "none";
       document.getElementById("id02").style.display = "none";
       document.getElementById("id03").style.display = "none";
@@ -165,32 +189,35 @@ var App = new Vue({
       this.formCambiarNombre = null;
     },
     opciones(item) {
-      console.log(item);
+      //habre modal opciones
       document.getElementById("id03").style.display = "block";
-      this.datoSeleccionado = item;
+      this.datoSeleccionado = item; //agrega los datos que seleccion a la variable global
     },
     eliminar() {
       if (this.datoSeleccionado != null) {
-        var tipo = this.datoSeleccionado.tipo;
-        var nombre = this.datoSeleccionado.nombre;
+        //si hay un dato seleccionado
+        var tipo = this.datoSeleccionado.tipo; //tipo del dato seleccionado
+        var nombre = this.datoSeleccionado.nombre; //nombre del dato
         if (tipo == "Directorio") {
           //elimiar tipo carpeta
-          fsxtra.remove(this.link.join("/") + "/" + nombre, err => {
+          fsxtra.remove(this.ruta.join("/") + "/" + nombre, err => {
+            //funcion para eliminar carpetas
             if (!err) {
-              this.recargar();
-              document.getElementById("id03").style.display = "none";
+              this.recargar(); //recarga la pagina
+              document.getElementById("id03").style.display = "none"; //cierra modl
             } else {
+              /* Error de algun tipo */
               alert("Error! \n" + "Error");
             }
           });
         } else {
           //eliminar tipo archivo
           try {
-            fs.unlinkSync(this.link.join("/") + "/" + nombre);
-            this.recargar();
+            fs.unrutaSync(this.ruta.join("/") + "/" + nombre); //funcion para eliminar archivos
+            this.recargar(); //recarga pagina
             document.getElementById("id03").style.display = "none";
           } catch (err) {
-            // handle the error
+            // algun tipo de error
             console.log(err);
             alert("Error! \n" + "Error");
           }
@@ -204,15 +231,17 @@ var App = new Vue({
         this.formCambiarNombre != null &&
         this.datoSeleccionado.nombre != null
       ) {
-        var nombreAnterior = this.datoSeleccionado.nombre;
-        var nuevoNombre = this.formCambiarNombre;
+        // si el campo de cambiar nombre no esta vacio
+        var nombreAnterior = this.datoSeleccionado.nombre; //nombre anterio
+        var nuevoNombre = this.formCambiarNombre; //nuevo nombre
+        //funcion para renombrrar
         fs.rename(
-          this.link.join("/") + "/" + nombreAnterior,
-          this.link.join("/") + "/" + nuevoNombre,
+          this.ruta.join("/") + "/" + nombreAnterior,
+          this.ruta.join("/") + "/" + nuevoNombre,
           err => {
             if (!err) {
               this.formCambiarNombre = null;
-              this.recargar();
+              this.recargar(); //recargar pagina
               document.getElementById("id03").style.display = "none";
             } else {
               alert("Error! \n" + "Nombre ya utilizado");
@@ -227,30 +256,32 @@ var App = new Vue({
     },
     copiar() {
       if (this.datoSeleccionado != null) {
-        var tipo = this.datoSeleccionado.tipo;
-        var nombre = this.datoSeleccionado.nombre;
-        if (tipo == "Directorio") {
-          this.lugarAnterior = this.link.join("/") + "/" + nombre;
-        } else {
-          this.lugarAnterior = this.link.join("/") + "/" + nombre;
-        }
+        //si hay un archivo seleccionado
+        var nombre = this.datoSeleccionado.nombre; //toma el nombre del dato que selecciono
+        //agrega la ruta del dato seleccionado para guardar globalmente cual archivo o carpeta sera modificado
+        this.lugarAnterior = this.ruta.join("/") + "/" + nombre;
       }
       document.getElementById("id03").style.display = "none";
       document.getElementById("pegar").style.display = "inline";
     },
     pegar() {
       if (this.datoSeleccionado != null) {
+        //si hay un archivo seleccionado
         var nombre = this.datoSeleccionado.nombre;
         var lugarAnterior = this.lugarAnterior;
-        fsxtra.copy(lugarAnterior, this.link.join("/") + "/" + nombre, err => {
+        fsxtra.copy(lugarAnterior, this.ruta.join("/") + "/" + nombre, err => {
+          //funcion para copiar
           if (!err) {
-            this.recargar();
-            document.getElementById("pegar").style.display = "none";
+            this.recargar(); //recarga pagina
+            document.getElementById("pegar").style.display = "none"; //quita modal
+            //inicializa variables globales
             this.datoSeleccionado = null;
             this.lugarAnterior = null;
           } else {
+            //si hay errores
             console.log(err);
             document.getElementById("pegar").style.display = "none";
+            //inicializa variables globales
             this.datoSeleccionado = null;
             this.lugarAnterior = null;
             alert(
--- js/utils.js
+++
@@ -1,6 +0,0 @@
-var fs = require("fs");
-var electron = require("electron");
-var ruta = require("path");
-/* var rmrf = require("rimraf"); */
-/* var mkdp = require("mkdirp"); */
-var fsxtra = require("fs-extra");
